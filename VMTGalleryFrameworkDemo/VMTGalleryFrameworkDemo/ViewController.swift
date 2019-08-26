1332e6e11423af71f631358264b663e3bea00682
Trinh Võ Mai <afterjob2018@gmail.com>
2019-08-26 16:45:54 +0700

    Update Multi Selection

--- VMTGalleryFrameworkDemo/VMTGalleryFrameworkDemo/ViewController.swift
+++ VMTGalleryFrameworkDemo/VMTGalleryFrameworkDemo/ViewController.swift
@@ -13,28 +13,47 @@ class ViewController: UIViewController, VMTGalleryViewDelegate {
 
     @IBOutlet weak var imgShowImageDemo: UIImageView!
     @IBOutlet weak var vAllImageBase: UIView!
+
+    var vGallery: VMTGalleryView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
+        customUI()
         initGalleryView()
 
     }
 
+    private func customUI() {
+        imgShowImageDemo.layer.borderColor = UIColor.gray.cgColor
+        imgShowImageDemo.layer.borderWidth = 2
+        imgShowImageDemo.layer.cornerRadius = 10
+        imgShowImageDemo.layer.masksToBounds = true
+    }
+
     func initGalleryView() {
         let r = CGRect(x: 0, y: 0, width: vAllImageBase.frame.size.width, height: vAllImageBase.frame.size.height)
-        let vGallery = VMTGalleryView(frame: r)
-        vGallery.backgroundColor = UIColor.white
-        vGallery.delegate = self
-        vAllImageBase.addSubview(vGallery)
+        vGallery = VMTGalleryView(frame: r)
+        vGallery?.backgroundColor = UIColor.white
+        vGallery?.delegate = self
+        vAllImageBase.addSubview(vGallery!)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let r = CGRect(x: 0, y: 0, width: vAllImageBase.frame.size.width, height: vAllImageBase.frame.size.height)
+        vGallery?.setFrame(frame: r)
     }
 
     // Did select image
     func didSelectImage(image: UIImage) {
         imgShowImageDemo.image = image
     }
-
+    // Select multi image
+    func didSelectMultiImage(arrImages: [UIImage]) {
+        print(arrImages.count)
+    }
 
 }
 
